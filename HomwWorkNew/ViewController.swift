f10eb795cc7171a129179d151543980a456fee14
Maksym <maksymstovolos2002@gmail.com>
2022-08-09 13:50:46 -0400

    Initial Commit

---
+++ HomwWorkNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomwWorkNew
+//
+//  Created by Max Stovolos on 8/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
